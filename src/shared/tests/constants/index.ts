deac41a9a24ff0cc3d1312c99f274fd2383724cc
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-11-25 10:44:21 -0300

    utils: constants uuids

--- src/shared/tests/constants/index.ts
+++ src/shared/tests/constants/index.ts
@@ -1,1 +1,3 @@
 export const validUuid = '9366b7dc-2d71-4799-b91c-c64adb205104'
+export const validUuid_2 = '4466b7dc-2d71-4799-b91c-c64adb205104'
+export const validUuid_3 = '4466b7dc-2d71-4799-b91c-c64adb206666'
